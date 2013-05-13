c3bde2c754d60e76b8d9cec09aceb363aab66e88
Eric Palmer <epalmer@richmond.edu>
2013-05-13 16:54:07 -0400

    Simple example of how to do a strings.Join()

---
+++ join.go
@@ -0,0 +1,17 @@
+package main
+
+import "strings"
+import "fmt"
+
+func main() {
+	var a,b,c string =  "A", "B", "C"
+	fmt.Println(a,b,c)
+
+
+	myStrArray := []string{a, b, c}
+	var myStr = strings.Join(myStrArray, "\t")
+	fmt.Println(myStr)
+	fmt.Println(strings.Join([]string{
+		"X", "Y"},":"))
+	
+}
