7cdebfb8399ae539f5c204e52fa70707009a34c6
Eric Palmer <epalmer@richmond.edu>
2013-05-13 16:58:34 -0400

    Gets some dir and file info but does not yet output it very well
    Trying to use tabwriter but it is not working as I would expect it.

--- dirlook.go
+++ dirlook.go
@@ -1,5 +1,7 @@
 /**
  * just print out the contents and some metadata about directory contents
+ *
+ * tabWriter does not yet work correctly
  **/
 
 package main
@@ -7,10 +9,18 @@ package main
 import "log"
 import "fmt"
 import iou "io/ioutil"
+import "text/tabwriter"
+import "os"
+ import "strings"
 
+//import "time"
 
  func main() {
  	dir :=  "/Volumes/HD2/temp"
+ 	w := new(tabwriter.Writer);
+	w.Init(os.Stdout, 31, 2, 2, ':', tabwriter.AlignRight)
+
+
 	fmt.Println(dir +"\n====================")
 	// dirArray []os.FileInfo
 	// error err
@@ -21,9 +31,25 @@ import iou "io/ioutil"
 	fmt.Print(len(dirInfo))
 	fmt.Println(" items in the folder " + dir)
 	fmt.Println("=========================================")
-	for i := range dirInfo {
-		println(dirInfo[i].IsDir(), dirInfo[i].Name() + " ")
+
+	for  i := range dirInfo {
+		var isDir string
+		if dirInfo[i].IsDir() {
+			isDir = "D" 
+		} else {
+			isDir = "-"
+		}
+		var myStr = strings.Join([]string{
+			isDir,
+			//string(dirInfo[i].Size()),
+			//string(dirInfo[i].Mode()),
+			//dirInfo[i].ModTime().String(),
+			dirInfo[i].Name()}, 
+			"\t")
+
+		fmt.Fprintln(w, myStr)	
 	}
+	w.Flush()
 
 
 }
