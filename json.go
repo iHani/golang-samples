3c2837ad2d970e81fa8a04ecd6298fec75cc8651
Eric Palmer <eric@ericfpalmer.com>
2013-05-23 09:05:28 -0400

    Added unmarshall example for json
    Still working on pritin out the keys and the values

---
+++ json.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+// not that unmarshal seems to choke on \n in the json string
+func main() {
+	fmt.Println("we will unmarshal json without knowing the data structure")
+
+	var f interface{}
+	err := json.Unmarshal(jsonByte(), &f)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(f)
+	prtKeys(f, 0)
+}
+
+func jsonByte() []byte {
+
+	return []byte(`{"parent":{"name":"a parent name","child1":{"type":"child","name":"child1","desc":"some desc of child 1"},"child2":{"type":"child","name":"child3","desc":"child2 has children","gchild1":{"prop1":"value1-a","prop2":"value2-a"},"gchild2":{"prop1":"value1-b","prop2":"value2-b"}},"child3":{"type":"child","name":"child3","desc":"some desc of child 3"}}}`)
+
+}
+func prtKeys(f interface{}, level int) {
+	mymap, ok := f.(map[string]interface{})
+	if !ok {
+		//fmt.Println("mymap, ok := f.(map[string]interface{}) NOT OK")
+		level = level - 1
+		return
+	} else {
+		//fmt.Println(mymap["parent"])
+	}
+
+	for k, n := range mymap {
+		_ = n
+		fmt.Println(level)
+		fmt.Println(strings.Repeat("   ", level) + k)
+		level = level + 1
+		prtKeys(n, level)
+	}
+
+}
